86690e76ca3a16a984184732615c4fa633ea04da
Rakesh Ran <143284882+rakeshran750@users.noreply.github.com>
2025-12-17 07:17:39 +0000

    update cors policy

--- be/app.js
+++ be/app.js
@@ -64,7 +64,13 @@ import transactionRoutes from "./routes/transaction.routes.js";
 
 const app = express();
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
 app.use(express.json());
 
 app.use("/api/dukan", dukanRoutes);
