383cff066a4e0703413c28c2d32ac9e57c655b10
Rakesh Ran <143284882+rakeshran750@users.noreply.github.com>
2025-12-17 07:27:43 +0000

    update Base_URL

--- fe/src/api/api.js
+++ fe/src/api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5001/api";
+// const BASE_URL = "http://localhost:5001/api";
+const BASE_URL = "https://udapay.onrender.com/api"
+
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
